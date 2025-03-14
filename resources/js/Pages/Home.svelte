bea88b81bd36e26b49bde61426d32ef798230ce9
Vincent Bergeron <vincent_bergeron98@hotmail.com>
2025-03-14 07:33:02 -0400

    WIP

--- resources/js/Pages/Home.svelte
+++ resources/js/Pages/Home.svelte
@@ -3,5 +3,5 @@
 </script>
 
 <div class="h-screen flex items-center justify-center">
-    <h1 class="text-3xl">Hello {$page.props.auth.user.name}</h1>
+    <h1 class="text-3xl">Hello, {$page.props.auth.user.name}</h1>
 </div>
